ef59e1ab6113cd5c2a4ff16293a0c594c1ef4d5e
Arunjot Singh <arunjot@antiersolutions.com>
2026-08-12 15:13:24 +0530

    chore: Restarting the repo with the new and better code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-use std::{sync::RwLock, thread};
-use std::sync::Arc;
-
 fn main() {
-    
-   let a = Arc::new(RwLock::new(10)); 
-    let b = Arc::clone(&a);
-    let c = Arc::clone(&a);
-
-   thread::spawn(move ||{
-   let b = b.read();
-   });
-   thread::spawn(move ||{
-   let mut b =  c.write().unwrap();
-   *b += 200;
-   });
-
-   println!("{:?}", a);
+    println!("Hello World");
 }
